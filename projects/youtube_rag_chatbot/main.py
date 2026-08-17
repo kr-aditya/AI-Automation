b83d96908486facbaa9162487b52dc6a68d16545
kr-aditya <aditya.kr09876@gmail.com>
2026-08-17 17:21:04 +0530

    chore: setup Groq LLM

---
+++ projects/youtube_rag_chatbot/main.py
@@ -0,0 +1,22 @@
+from groq import Groq
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+response = client.chat.completions.create(
+    model="openai/gpt-oss-120b",
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain RAG in one sentence."
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
